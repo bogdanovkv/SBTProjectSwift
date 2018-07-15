f8567ea22d82c11ff1163cc9b9c90df16fa1f2db
Константин Богданов <bogdakos@mail.ru>
2018-07-15 17:10:35 +0300

    LocationVC

---
+++ SBTSwiftProject/SBTSwiftProject/LocationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SBTSwiftProject
+//
+//  Created by Константин Богданов on 14.07.2018.
+//  Copyright © 2018 Константин Богданов. All rights reserved.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.green
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
